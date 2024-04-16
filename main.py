6dd70b5a9daa466c0ab2b7a72585ad8054201542
RiggityRob <rgoodeve@gmail.com>
2024-04-16 14:02:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from llama_cpp import Llama
 
 
 # GLOBAL VARIABLES
-my_model_path = "./zephyr-7b-beta.Q4_0.gguf"
+my_model_path = "./deepseek-coder-6.7b-instruct.Q6_K.gguf"
 CONTEXT_SIZE = 2048
 
 
 # LOAD THE MODEL
-zephyr_model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE, chat_format='llama-2')
+init_model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE, chat_format='llama-2')
 
 
 def generate_text_from_prompt(
@@ -15,7 +15,7 @@ def generate_text_from_prompt(
 ):
 
     # Define the parameters
-    model_output = zephyr_model(
+    model_output = init_model(
         user_prompt,
         max_tokens=max_tokens,
         temperature=temperature,
