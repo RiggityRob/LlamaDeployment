e3699c7854adf7730370044401c1cb49c4408f27
RiggityRob <rgoodeve@gmail.com>
2024-04-17 19:10:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ CONTEXT_SIZE = 2048
 
 
 # LOAD THE MODEL
-init_model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE, split_mode=llama_cpp.LLAMA_SPLIT_MODE_NONE, n_gpu_layers=10, chat_format='llama-2')
+init_model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE, split_mode=0, n_gpu_layers=10, chat_format='llama-2')
 
 
 def generate_text_from_prompt(
