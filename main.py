d55302da5e553d7c4d63c060817b29571d7895a2
RiggityRob <rgoodeve@gmail.com>
2025-03-04 08:45:57 -0800

    add
    add

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ init_model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE, chat_format='ll
 
 
 def generate_text_from_prompt(
-    user_prompt, max_tokens=3000, temperature=0.3, top_p=0.1, echo=True, stop=["Q"]
+    user_prompt, max_tokens=3000, temperature=0.3, top_p=0.5, echo=True, stop=["Q"]
 ):
 
     # Define the parameters
